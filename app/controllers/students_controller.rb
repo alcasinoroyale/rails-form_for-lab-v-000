b42cb6b0cc17c6982688d16516f9fc977a0af285
Alex Zdatny <alexzdatny@gmail.com>
2018-09-12 22:39:27 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,7 +7,7 @@ class StudentController < ApplicationController
   def create
     @student = Student.new(params.require(:student).permit(:first_name, :last_name))
     @student.save
-    redirect_to student_paht(@student)
+    redirect_to student_path(@student)
   end
 
   def show
